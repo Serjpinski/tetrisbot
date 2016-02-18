1e10fefd80a86ed03fb17423080d571a66f50224
Sergio Larrodera <serjpinski@gmail.com>
2016-02-18 02:55:42 +0100

    Added test for piece prediction

--- src/logic/Test.java
+++ src/logic/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 	public static void main (String[] args) {
 
-		testClassic2(true);
+		testClassicPred(1, true);
 	}
 	
 	/**
@@ -64,11 +64,67 @@ public class Test {
 			iter++;
 		}
 	}
+	
+	/**
+	 * Tests the classic bot with only active piece plus piece prediction.
+	 */
+	public static void testClassicPred(int predDepth, boolean relaxMode) {
+
+		Random rand = new Random();
+
+		int iter = 1;
+		long totalLines = 0;
+		int minLines = -1;
+		int maxLines = -1;
+		long totalMoves = 0;
+		double totalMoveTime = 0;
+
+		while (true) {
+
+			boolean[][] grid = Grid.emptyGrid();
+
+			int lines = 0;
+
+			long t0 = System.nanoTime();
+			Move best = bot.classic.Bot.search(predDepth, grid, rand.nextInt(7));
+			long t1 = System.nanoTime() - t0;
+
+			while (best != null) {
+
+				totalMoveTime += t1 / 1000000.0; // Move time in ms
+				totalMoves++;
+
+				best.place(grid);
+				lines += best.getLinesCleared();
+
+				Grid.printGrid(grid);
+				System.out.println("[Lines: " + lines + "]");
+				System.out.println("[Iteration: " + iter + "]");
+				System.out.println("[Avg lines: " + totalLines / (double) (iter - 1) + "]");
+				System.out.println("[Min lines: " + minLines + "]");
+				System.out.println("[Max lines: " + maxLines + "]");
+				System.out.println("[Avg move time: " + totalMoveTime / totalMoves + "]");
+				System.out.println();
+
+				if (relaxMode) try { Thread.sleep(1); } catch (InterruptedException e) {}
+
+				t0 = System.nanoTime();
+				best = bot.classic.Bot.search(predDepth, grid, rand.nextInt(7));
+				t1 = System.nanoTime() - t0;
+			}
+
+			if (lines > maxLines) maxLines = lines;
+			if (minLines == -1 || lines < minLines) minLines = lines;
+			totalLines += lines;
+
+			iter++;
+		}
+	}
 
 	/**
 	 * Tests the classic bot with both active and next piece.
 	 */
-	public static void testClassic2(boolean relaxMode) {
+	public static void testClassicNext(boolean relaxMode) {
 
 		Random rand = new Random();
 
