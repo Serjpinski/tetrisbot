92649625b8e56385468df8ba455f50c37f611871
Sergio Larrodera <serjpinski@gmail.com>
2016-06-06 21:14:05 +0200

    Added cumulative probability candidate selection for hybrid AI

--- src/neural/NeuralAI.java
+++ src/neural/NeuralAI.java
@@ -1,8 +1,11 @@
 package neural;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 import core.Grid;
+import core.Misc;
 import core.Move;
 import core.Test;
 import hex.genmodel.easy.EasyPredictModelWrapper;
@@ -12,7 +15,8 @@ import hex.genmodel.easy.prediction.MultinomialModelPrediction;
 
 public class NeuralAI {
 
-	private static final double MIN_PROB = 0.00001;
+	private static final double HYBRID_PROB = 0.01;
+	private static final boolean HYBRID_CUMULATIVE = true;
 
 	private EasyPredictModelWrapper[] models;
 
@@ -52,24 +56,47 @@ public class NeuralAI {
 				double[] probs = prediction.classProbabilities;
 				ArrayList<Move> candidates = new ArrayList<Move>();
 
-				for (int i = 0; i < probs.length; i++) {
+				if (HYBRID_CUMULATIVE) {
 
-					if (probs[i] >= MIN_PROB) {
+					TreeMap<Double,Integer> moves = new TreeMap<Double,Integer>();
 
-						int code = Integer.parseInt(models[activePiece].getResponseDomainValues()[i]);
-						Move move = Move.code2Move(code, activePiece, grid);
+					for (int i = 0; i < probs.length; i++) {
+
+						while (moves.containsKey(probs[i])) probs[i] -= Double.MIN_VALUE;
+						moves.put(probs[i], Integer.parseInt(models[activePiece].getResponseDomainValues()[i]));
+					}
+
+					double cumulativeProb = 0;
+
+					while (!moves.isEmpty() && (cumulativeProb < 1 - HYBRID_PROB || candidates.isEmpty())) {
+
+						Map.Entry<Double,Integer> probMove = moves.pollLastEntry();
+						Move move = Move.code2Move(probMove.getValue(), activePiece, grid);
+						cumulativeProb += probMove.getKey();
 						if (move != null) candidates.add(move);
 					}
 				}
-				
+				else {
+
+					for (int i = 0; i < probs.length; i++) {
+
+						if (probs[i] >= HYBRID_PROB) {
+
+							int code = Integer.parseInt(models[activePiece].getResponseDomainValues()[i]);
+							Move move = Move.code2Move(code, activePiece, grid);
+							if (move != null) candidates.add(move);
+						}
+					}
+				}
+
 				Test.HYBRID_EVAL_CALL_FREQS[candidates.size()]++;
 
 				if (candidates.isEmpty()) return Move.code2Move(Integer.parseInt(prediction.label), activePiece, grid);
 
 				Move best = candidates.get(0);
-				
+
 				if (candidates.size() == 1) return best;
-				
+
 				best.place(grid);
 				best.setScore(heuristic.HeuristicAI.evalPred(false, grid, null, predDepth));
 				best.remove(grid);
@@ -77,19 +104,19 @@ public class NeuralAI {
 				for (int i = 1; i < candidates.size(); i++) {
 
 					Move move = candidates.get(i);
-					
+
 					move.place(grid);
 					move.setScore(heuristic.HeuristicAI.evalPred(false, grid, null, predDepth));
 					move.remove(grid);
-					
+
 					if (move.getScore() < best.getScore()) best = move;
 				}
-				
+
 				Test.HYBRID_EVAL_CALLS += candidates.size();
 
 				return best;
 			}
-			
+
 			return Move.code2Move(Integer.parseInt(prediction.label), activePiece, grid);
 		}
 		catch (PredictException e) {
